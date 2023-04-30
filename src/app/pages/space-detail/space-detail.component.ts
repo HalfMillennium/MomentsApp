a70b715602e61ec8404fdd207d8705bdca72329d
HalfMillennium <glc2266@gmail.com>
2023-04-30 16:20:33 -0400

    Surround carousel with mat-card, and add mat-card-action button for viewing space 'angles' content

--- src/app/pages/space-detail/space-detail.component.ts
+++ src/app/pages/space-detail/space-detail.component.ts
@@ -23,7 +23,6 @@ export class SpaceDetail implements OnDestroy {
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {
     const params = this.activatedRoute.snapshot.paramMap;
-    const spaceId = params.get('id');
     // fetch spaceInfo using spaceId - using test data for now
     this.currentSpace = {
       id: params.get('id'),
