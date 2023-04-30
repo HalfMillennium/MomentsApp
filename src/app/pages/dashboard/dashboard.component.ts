0d8621612c31799d24bcec10035e788067b418de
HalfMillennium <glc2266@gmail.com>
2023-04-30 13:58:05 -0400

    Refactor mock data and update typings for google-trends-api

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MaterialModule} from '../../../material.module';
-import { SpaceInfo } from '../../utils/interfaces';
+import { MOCK_SPACES } from '../../utils/resources';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
@@ -12,24 +12,8 @@ import { Router, RouterModule } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class Dashboard {
+  readonly MOCK_SPACES = MOCK_SPACES;
+
   constructor(private router: Router) {}
-  mockSpaces: SpaceInfo[] = [{
-    id: "12",
-    title: "Kenyorgi Dogs",
-    desc: "New dog breed discovered in South Kenya",
-    images: ["https://images.theconversation.com/files/465656/original/file-20220527-17-9sel6k.jpg?ixlib=rb-1.1.0&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip"],
-    alt: "Photo of 'Kenyorgi' dog"
-  },{
-    id: "13",
-    title: "Google Magi",
-    desc: "Google unveils new AI technology to improve search",
-    images: ["https://wgmimedia.com/wp-content/uploads/2023/04/Google-Magi.jpg"],
-    alt: "Logo of Google Magi"
-  }, {
-    id: "14",
-    title: "Frank Ocean",
-    desc: "Frank Ocean stuns Coachella with confusingly pathetic set",
-    images: ["https://static.stereogum.com/uploads/2023/04/frank-ocean-coachella-1681738729-1000x750.jpg"],
-    alt: "Photo of Frank Ocean concert"
-  }];
+  
 }
