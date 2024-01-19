1cb3b8d49dd8d2e7c4071cfbe50038b2f9860294
HalfMillennium <glc2266@gmail.com>
2024-01-19 13:13:04 -0500

    Clear credentials from Firebase config

--- src/app/shared/common/config/firebase.ts
+++ src/app/shared/common/config/firebase.ts
@@ -1,10 +1,10 @@
 /** Firebase config data */
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyCkquqIoFFml9TloU_dHzPRzbhea91VTgw',
-  authDomain: 'momentsapp-c9b78.firebaseapp.com',
-  projectId: 'momentsapp-c9b78',
-  storageBucket: 'momentsapp-c9b78.appspot.com',
-  messagingSenderId: '956383996712',
-  appId: '1:956383996712:web:96484dc47570bbbb3b7e1d',
-  measurementId: 'G-VBEZ7H9N4R',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
